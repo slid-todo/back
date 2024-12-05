6f88eb932bef9b790ffd10ad487b2c3c3312c0b1
bbbbooo <qlqlzbxkzl@naver.com>
2024-12-06 01:05:35 +0900

    feat : 어노테이션 추가

--- src/main/java/com/codeit/todo/repository/UserRepository.java
+++ src/main/java/com/codeit/todo/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.codeit.todo.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository {
 
 }
--- src/main/java/com/codeit/todo/web/controller/AuthController.java
+++ src/main/java/com/codeit/todo/web/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.codeit.todo.web.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 
 }
