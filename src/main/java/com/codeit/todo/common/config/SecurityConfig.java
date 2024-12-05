1f02a3dbab86de81f58506fb902d5b36fc752f6c
bbbbooo <qlqlzbxkzl@naver.com>
2024-12-06 01:02:27 +0900

    feat : 스웨거 및 시큐리티 설정 추가

--- src/main/java/com/codeit/todo/common/config/SecurityConfig.java
+++ src/main/java/com/codeit/todo/common/config/SecurityConfig.java
@@ -1,5 +1,27 @@
 package com.codeit.todo.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/login").permitAll()
+                    .requestMatchers("/swagger/**").permitAll()
+                    .requestMatchers("/**").permitAll()
+                    .anyRequest().permitAll()
+//                    .anyRequest().authenticated() // 인증 구현시, 주석 해제
+            );
+        return http.build();
+    }
 }
