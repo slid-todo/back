8e1667279a145798317caaf7e3a2a57e23937a63
bbbbooo <qlqlzbxkzl@naver.com>
2024-12-05 22:40:35 +0900

    refactor : 배포 시, logs 폴더에 연-월-일 순으로 로그를 남기도록 수정

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 APP_DIR="${APP_DIR:-/home/ec2-user/todo}"
 JAR_PATH="${APP_DIR}/todo-0.0.1-SNAPSHOT.jar"
+LOG_DIR="${APP_DIR}/logs"
+DEPLOY_DATE=$(date +'%Y-%m-%d')
+
+mkdir -p "$LOG_DIR"
 
 PID=$(lsof -t -i:8080)
 if [ ! -z "$PID" ]; then
@@ -9,6 +13,8 @@ if [ ! -z "$PID" ]; then
   kill -9 $PID
 fi
 
+LOG_FILE="${LOG_DIR}/todo-${DEPLOY_DATE}.log"
+
 echo "Starting new application..."
-nohup java -jar "$JAR_PATH" > todo.log 2>&1 &
+nohup java -jar "$JAR_PATH" > "$LOG_FILE" 2>&1 &
 echo "Application started!"
