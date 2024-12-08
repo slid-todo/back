9a68b192279f474cbcf05c2f7f6f989dbf3c0a20
soheeparklee <soheeparklee@gmail.com>
2024-12-08 13:48:47 +0100

    feat: 로그인 컨트롤러, 서비스

--- src/main/java/com/codeit/todo/repository/UserRepository.java
+++ src/main/java/com/codeit/todo/repository/UserRepository.java
@@ -1,6 +1,14 @@
 package com.codeit.todo.repository;
 
 
-public interface UserRepository {
+import com.codeit.todo.domain.Todo;
+import com.codeit.todo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/codeit/todo/service/user/UserService.java
+++ src/main/java/com/codeit/todo/service/user/UserService.java
@@ -1,5 +1,8 @@
 package com.codeit.todo.service.user;
 
+import com.codeit.todo.web.dto.request.auth.LoginRequest;
+
 public interface UserService {
 
+    String login(LoginRequest loginRequest);
 }
