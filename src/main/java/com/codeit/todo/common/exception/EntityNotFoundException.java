e156c550522f25fffb324af54402f3f6d73ac754
bbbbooo <hyeons1213@gmail.com>
2024-12-10 06:58:06 +0900

    fix : EntityNotFoundException 내 필드 타입을 int -> string으로 변경

--- src/main/java/com/codeit/todo/common/exception/EntityNotFoundException.java
+++ src/main/java/com/codeit/todo/common/exception/EntityNotFoundException.java
@@ -16,17 +16,17 @@ public class EntityNotFoundException extends ApplicationException{
     private static final String ENTITY_NOT_FOUND_EXCEPTION_MESSAGE = "해당하는 엔티티를 찾을 수 없습니다.";
     private static final int ENTITY_NOT_FOUND_EXCEPTION_STATUS_CODE = 404;
 
-    private final int entityId;
+    private final String request;
     private final String entityType;
 
     /**
      *
-     * @param entityId 엔티티 ID
+     * @param request 엔티티를 찾기 위해 요청한 값
      * @param entityType 엔티티 타입 (User 등)
      */
-    public EntityNotFoundException(int entityId, String entityType) {
+    public EntityNotFoundException(String request, String entityType) {
         super(new ErrorStatus(ENTITY_NOT_FOUND_EXCEPTION_MESSAGE, ENTITY_NOT_FOUND_EXCEPTION_STATUS_CODE, LocalDateTime.now()));
-        this.entityId = entityId;
+        this.request = request;
         this.entityType = entityType;
     }
 }
