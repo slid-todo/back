1b997d464e797e3a48109f1ce108b5b452007bd5
bbbbooo <hyeons1213@gmail.com>
2024-12-13 17:44:06 +0900

    fix : 버전업으로 인한 cors 설정 변경

---
+++ src/main/java/com/codeit/todo/common/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.codeit.todo.common.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CorsConfig {
+
+    public static CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://front-sooty-three.vercel.app");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
