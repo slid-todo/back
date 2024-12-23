061cf3e9958f7e34e77e0daeea8065808cb35a61
soheeparklee <soheeparklee@gmail.com>
2024-12-23 10:02:28 +0100

    feat: 좋아요, 댓글 엔티티 작성

---
+++ src/main/java/com/codeit/todo/domain/Comment.java
@@ -0,0 +1,31 @@
+package com.codeit.todo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int commentId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "complete_id", nullable = false)
+    private Complete complete;
+}
---
+++ src/main/java/com/codeit/todo/domain/Likes.java
@@ -0,0 +1,23 @@
+package com.codeit.todo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int likesId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "complete_id", nullable = false)
+    private Complete complete;
+}
