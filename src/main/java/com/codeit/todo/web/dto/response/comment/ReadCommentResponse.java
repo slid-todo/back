43c5a804cb3cbe3f34dc2754bbc9971ebc3b473e
bbbbooo <hyeons1213@gmail.com>
2025-01-05 17:52:03 +0900

    feat : 인증 상세조회 응답 필드 추가

--- src/main/java/com/codeit/todo/web/dto/response/comment/ReadCommentResponse.java
+++ src/main/java/com/codeit/todo/web/dto/response/comment/ReadCommentResponse.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 @Builder
 public record ReadCommentResponse(
         int commentId,
+        int userId,
         String content,
         String userName,
         String profileImage,
@@ -16,6 +17,7 @@ public record ReadCommentResponse(
     public static ReadCommentResponse fromEntity(Comment comment) {
         return ReadCommentResponse.builder()
                 .commentId(comment.getCommentId())
+                .userId(comment.getUser().getUserId())
                 .content(comment.getContent())
                 .userName(comment.getUser().getName())
                 .profileImage(comment.getUser().getProfilePic())
